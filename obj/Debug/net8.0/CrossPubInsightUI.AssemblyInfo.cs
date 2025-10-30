7ef37e0da2a3d286f78bfb6ff01f185cc90670f6
adunseayorinde <adunseayorinde@gmail.com>
2025-10-30 09:08:02 +0100

    removed unused nav bar

--- obj/Debug/net8.0/CrossPubInsightUI.AssemblyInfo.cs
+++ obj/Debug/net8.0/CrossPubInsightUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CrossPubInsightUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1764f248576446d4f1ec52d8c246be9861691cd6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b132555b088de1417c179d695248e5891241ee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CrossPubInsightUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CrossPubInsightUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
